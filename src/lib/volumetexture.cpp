8f0492b9ab2dc4e5134e6342ed8fc38abdef02ae
Ivan Komissarov <ABBAPOH@gmail.com>
2015-05-29 10:01:16 +0300

    Add useful methods for VolumeTexture

--- src/lib/volumetexture.cpp
+++ src/lib/volumetexture.cpp
@@ -5,9 +5,51 @@ VolumeTexture::VolumeTexture()
 
 }
 
+int VolumeTexture::width() const
+{
+    return _size.width();
+}
+
+int VolumeTexture::height() const
+{
+    return _size.height();
+}
+
 int VolumeTexture::depth() const
 {
     return _images.size();
+}
+
+QRgb VolumeTexture::pixel(int x, int y, int z)
+{
+    if (x >= width() || y >= height() || z >= depth())
+        return QRgb();
+
+    return _images.at(z).pixel(x, y);
+}
+
+void VolumeTexture::setPixel(int x, int y, int z, uint index_or_rgb)
+{
+    if (x >= width() || y >= height() || z >= depth())
+        return;
+
+    _images[z].setPixel(x, y, index_or_rgb);
+}
+
+uchar *VolumeTexture::scanLine(int y, int z)
+{
+    if (y >= height() || z >= depth())
+        return Q_NULLPTR;
+
+    return _images[z].scanLine(y);
+}
+
+const uchar *VolumeTexture::scanLine(int y, int z) const
+{
+    if (y >= height() || z >= depth())
+        return Q_NULLPTR;
+
+    return _images[z].scanLine(y);
 }
 
 QImage VolumeTexture::slice(int index)
@@ -20,7 +62,9 @@ QImage VolumeTexture::slice(int index)
 
 void VolumeTexture::addSlice(const QImage &image)
 {
-    _images.append(image);
+    if (_size.isEmpty())
+        _size = image.size();
+    _images.append(image.scaled(_size));
 }
 
 void VolumeTexture::removeSlice(int index)
@@ -34,5 +78,7 @@ void VolumeTexture::setSlice(int index, const QImage &image)
 {
     if (index < 0 || index >= depth())
         return;
-    _images[index] = image;
+    if (_size.isEmpty())
+        _size = image.size();
+    _images[index] = image.scaled(_size);
 }
--- src/lib/volumetexture.h
+++ src/lib/volumetexture.h
@@ -8,14 +8,28 @@ class VolumeTexture
 public:
     VolumeTexture();
 
+    QImage::Format format() const;
+
+    int width() const;
+    int height() const;
     int depth() const;
+
+    QRgb pixel(int x, int y, int z);
+    void setPixel(int x, int y, int z, uint index_or_rgb);
+
+    uchar *scanLine(int y, int z);
+    const uchar *scanLine(int y, int z) const;
 
     QImage slice(int slice);
     void addSlice(const QImage &image);
     void removeSlice(int index);
     void setSlice(int index, const QImage &image);
 
+    // VolumeTexture scaled(int width, int height, int depth);
+    // VolumeTexture transformed(const QTransform &matrix, Qt::TransformationMode mode = Qt::FastTransformation) const
+
 private:
+    QSize _size;
     QVector<QImage> _images;
 };
 
