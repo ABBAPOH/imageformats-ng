427eb40c3a56e90618cda0a28b49b57dcbff3f47
Ivan Komissarov <ABBAPOH@gmail.com>
2015-07-14 16:15:42 +0300

    Remove useless code from main.cpp

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 
 #include <QtWidgets/QApplication>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -9,19 +8,6 @@ int main(int argc, char *argv[])
 
     MainWindow w;
     w.show();
-
-    if (QTypeInfo<QImage>::isComplex)
-        qDebug() << "complex";
-    if (QTypeInfo<QImage>::isDummy)
-        qDebug() << "dummy";
-    if (QTypeInfo<QImage>::isIntegral)
-        qDebug() << "integral";
-    if (QTypeInfo<QImage>::isLarge)
-        qDebug() << "large";
-    if (QTypeInfo<QImage>::isPointer)
-        qDebug() << "pointer";
-    if (QTypeInfo<QImage>::isStatic)
-        qDebug() << "static";
 
     return app.exec();
 }
