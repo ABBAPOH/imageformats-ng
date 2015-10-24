60912f566e46286bf2252a8c4beb9ec110ea1719
Ivan Komissarov <ABBAPOH@gmail.com>
2015-10-24 16:47:40 +0300

    Remove unused methods

--- src/libs/imagedocument/imageexifmeta.cpp
+++ src/libs/imagedocument/imageexifmeta.cpp
@@ -1,23 +1,5 @@
 #include "imageexifmeta.h"
 
-template<typename T>
-static Optional<T> getInt(const ImageExifMeta::Values &values, ImageExifMeta::Tag tag)
-{
-    const auto it = values.find(tag);
-    if (it == values.end())
-        return Nothing();
-    return T(it->toInt());
-}
-
-template<typename T>
-static void set(ImageExifMeta::Values &values, ImageExifMeta::Tag tag, const Optional<T> &value)
-{
-    if (value)
-        values.insert(tag, *value);
-    else
-        values.remove(tag);
-}
-
 QVariant ImageExifMeta::value(Tag option) const
 {
     return _values.value(option);
