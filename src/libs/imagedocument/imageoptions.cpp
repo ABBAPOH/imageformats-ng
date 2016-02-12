9132fb4d3f66a57d5d467691854adfe5488f8bce
Комиссаров Иван Владимирович <i.komissarov@2Gis.ru>
2016-02-12 10:28:14 +0600

    Add ImageOptions class

---
+++ src/libs/imagedocument/imageoptions.cpp
@@ -0,0 +1,90 @@
+#include "imageoptions.h"
+
+ImageOptions::ImageOptions()
+{
+}
+
+bool ImageOptions::isEmpty() const
+{
+    return _options.isEmpty();
+}
+
+QRect ImageOptions::clipRect() const
+{
+    return _options.value(ClipRect, QRect()).toRect();
+}
+
+void ImageOptions::setClipRect(const QRect &rect)
+{
+    _options.insert(ClipRect, rect);
+}
+
+QSize ImageOptions::scaledSize() const
+{
+    return _options.value(ScaledSize, QSize()).toSize();
+}
+
+void ImageOptions::setScaledSize(const QSize &size)
+{
+    _options.insert(ScaledSize, size);
+}
+
+QRect ImageOptions::scaledCliptRect() const
+{
+    return _options.value(ScaledClipRect, QRect()).toRect();
+}
+
+void ImageOptions::setScaledCliptRect(const QRect &rect)
+{
+    _options.insert(ScaledClipRect, rect);
+}
+
+int ImageOptions::inputQuality() const
+{
+    return _options.value(InputQuality, -1).toInt();
+}
+
+void ImageOptions::setInputQuality(int quality)
+{
+    _options.insert(InputQuality, quality);
+}
+
+QColor ImageOptions::backgroundColor() const
+{
+    return _options.value(BackgroundColor).value<QColor>();
+}
+
+void ImageOptions::setBackgroundColor(const QColor &color)
+{
+    _options.insert(BackgroundColor, color);
+}
+
+int ImageOptions::compression() const
+{
+    return _options.value(CompressionRatio, -1).toInt();
+}
+
+void ImageOptions::setCompression(int compression)
+{
+    _options.insert(CompressionRatio, compression);
+}
+
+int ImageOptions::gamma() const
+{
+    return _options.value(Gamma, -1).toInt();
+}
+
+void ImageOptions::setGamma(int gamma)
+{
+    _options.insert(Gamma, gamma);
+}
+
+int ImageOptions::quality() const
+{
+    return _options.value(Quality, -1).toInt();
+}
+
+void ImageOptions::setQuality(int quality)
+{
+    _options.insert(Quality, quality);
+}
---
+++ src/libs/imagedocument/imageoptions.h
@@ -0,0 +1,60 @@
+#ifndef IMAGEOPTIONS_H
+#define IMAGEOPTIONS_H
+
+#include <QtCore/QHash>
+#include <QtCore/QRect>
+#include <QtCore/QVariant>
+#include <QtGui/QColor>
+
+class ImageOptions
+{
+public:
+    ImageOptions();
+
+    enum Option {
+        // read options
+        ClipRect,
+        ScaledSize,
+        ScaledClipRect,
+        InputQuality,
+        BackgroundColor,
+
+        // write options
+        CompressionRatio,
+        Gamma,
+        Quality,
+        SubType
+    };
+    Q_ENUMS(Option)
+
+    bool isEmpty() const;
+
+    QRect clipRect() const;
+    void setClipRect(const QRect &rect);
+
+    QSize scaledSize() const;
+    void setScaledSize(const QSize &size);
+
+    QRect scaledCliptRect() const;
+    void setScaledCliptRect(const QRect &rect);
+
+    int inputQuality() const;
+    void setInputQuality(int quality);
+
+    QColor backgroundColor() const;
+    void setBackgroundColor(const QColor &color);
+
+    int compression() const;
+    void setCompression(int compression);
+
+    int gamma() const;
+    void setGamma(int gamma);
+
+    int quality() const;
+    void setQuality(int quality);
+
+private:
+    QHash<Option, QVariant> _options;
+};
+
+#endif // IMAGEOPTIONS_H
