30a228bab71058fe1e24aa54f3921c36f436a1a7
Ivan Komissarov <ABBAPOH@gmail.com>
2016-08-19 22:26:24 +0300

    Add missed doc

--- src/libs/utils/document.cpp
+++ src/libs/utils/document.cpp
@@ -19,6 +19,13 @@ Document::~Document()
 {
 }
 
+/*!
+    \property Document::isModified
+    This property holds whether the document has been modified by the user.
+    By default, this property is false.
+    \sa modificationChanged()
+*/
+
 bool Document::isModified() const
 {
     Q_D(const Document);
@@ -34,6 +41,20 @@ void Document::setModified(bool modified)
     d->modified = modified;
     emit modificationChanged(modified);
 }
+
+/*!
+    \fn Document::modificationChanged(bool changed)
+
+    This signal is emitted whenever the content of the document
+    changes in a way that affects the modification state. If \a
+    changed is true, the document has been modified; otherwise it is
+    false.
+
+    For example, calling setModified(false) on a document and then
+    changing a document causes the signal to get emitted. If you undo that
+    operation, causing the document to return to its original
+    unmodified state, the signal will get emitted again.
+*/
 
 /*!
     Constructs an AbstractDocument with the given \a parent and private class \a dd.
