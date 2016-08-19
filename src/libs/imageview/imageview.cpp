043ca60119a8d21d414e2d5345b39b6c69eba363
Ivan Komissarov <ABBAPOH@gmail.com>
2016-08-20 01:03:56 +0300

    Add missed call

--- src/libs/imageview/imageview.cpp
+++ src/libs/imageview/imageview.cpp
@@ -37,6 +37,7 @@ void ImageViewPrivate::init()
                      q, &ImageView::onViewPortSizeChanged);
 
     createActions();
+    retranslateUi();
 }
 
 void ImageViewPrivate::createActions()
