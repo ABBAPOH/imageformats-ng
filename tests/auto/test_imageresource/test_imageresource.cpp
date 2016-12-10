82bfe448d0d848082cc77e3bca2b965df764adee
Ivan Komissarov <ABBAPOH@gmail.com>
2016-12-11 02:46:00 +0300

    More tests for ImageResource

--- tests/auto/test_imageresource/test_imageresource.cpp
+++ tests/auto/test_imageresource/test_imageresource.cpp
@@ -9,6 +9,8 @@ private slots:
     void image();
     void cubeTexture();
     void volumeTexture();
+    void copyConstruct();
+    void moveConstruct();
 };
 
 void TestImageResource::defaultConstructed()
@@ -72,6 +74,58 @@ void TestImageResource::volumeTexture()
     QVERIFY(res.image().isNull());
     QVERIFY(res.cubeTexture().isNull());
     QCOMPARE(res.volumeTexture(), tex);
+}
+
+void TestImageResource::copyConstruct()
+{
+    QImage image(64, 64, QImage::Format_ARGB32);
+    image.fill(Qt::white);
+
+    ImageResource res1;
+    ImageResource res2(image);
+
+    ImageResource copy1(res1);
+    QVERIFY(copy1.isNull());
+    QCOMPARE(copy1.type(), ImageResource::Type::Invalid);
+    QVERIFY(copy1.image().isNull());
+    QVERIFY(copy1.cubeTexture().isNull());
+    QVERIFY(copy1.volumeTexture().isNull());
+
+    ImageResource copy2(res2);
+    QVERIFY(!copy2.isNull());
+    QCOMPARE(copy2.type(), ImageResource::Type::Image);
+    QCOMPARE(copy2.image(), image);
+    QVERIFY(copy2.cubeTexture().isNull());
+    QVERIFY(copy2.volumeTexture().isNull());
+}
+
+void TestImageResource::moveConstruct()
+{
+    QImage image(64, 64, QImage::Format_ARGB32);
+    image.fill(Qt::white);
+
+    ImageResource res1;
+    ImageResource res2(image);
+
+    ImageResource copy1(std::move(res1));
+    QVERIFY(copy1.isNull());
+    QCOMPARE(copy1.type(), ImageResource::Type::Invalid);
+    QVERIFY(copy1.image().isNull());
+    QVERIFY(copy1.cubeTexture().isNull());
+    QVERIFY(copy1.volumeTexture().isNull());
+
+    ImageResource copy2(std::move(res2));
+    QVERIFY(!copy2.isNull());
+    QCOMPARE(copy2.type(), ImageResource::Type::Image);
+    QCOMPARE(copy2.image(), image);
+    QVERIFY(copy2.cubeTexture().isNull());
+    QVERIFY(copy2.volumeTexture().isNull());
+
+    QVERIFY(res2.isNull());
+    QCOMPARE(res2.type(), ImageResource::Type::Invalid);
+    QVERIFY(res2.image().isNull());
+    QVERIFY(res2.cubeTexture().isNull());
+    QVERIFY(res2.volumeTexture().isNull());
 }
 
 QTEST_MAIN(TestImageResource)
