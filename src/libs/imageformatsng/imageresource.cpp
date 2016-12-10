43c96bcfc013b92d66a7992213cd2cfb0ad0e08c
Ivan Komissarov <ABBAPOH@gmail.com>
2016-12-11 02:22:02 +0300

    Add doc for ImageResource

--- src/libs/imageformatsng/imageresource.cpp
+++ src/libs/imageformatsng/imageresource.cpp
@@ -2,44 +2,87 @@
 
 #include <QtCore/QMap>
 
+/*!
+    \class ImageResource
+    ImageResource is a variant of three types - QImage, CubeTexture and VolumeTexture.
+*/
+
+/*!
+    \enum ImageResource::Type
+    This enum describes type stored in a resource.
+
+    \var ImageResource::Image
+    Resource contains a QImage.
+
+    \var ImageResource::Cubetexture
+    Resource contains a CubeTexture.
+
+    \var ImageResource::VolumeTexture
+    Resource contains a VolumeTexture.
+*/
+
+/*!
+    Constructs a null resource with an invalid type.
+*/
 ImageResource::ImageResource() Q_DECL_NOEXCEPT :
     _type(Type::Invalid)
 {
 }
 
+/*!
+    Constructs resource with the given \a image.
+*/
 ImageResource::ImageResource(const QImage &image) :
     _type(Type::Image),
     _image(image)
 {
 }
 
+/*!
+    Constructs resource with the given \a texture.
+*/
 ImageResource::ImageResource(const CubeTexture &texture) :
     _type(Type::CubeTexture),
     _cubeTexture(texture)
 {
 }
 
+/*!
+    Constructs resource with the given \a texture.
+*/
 ImageResource::ImageResource(const VolumeTexture &texture) :
     _type(Type::VolumeTexture),
     _volumeTexture(texture)
 {
 }
 
+/*!
+    Copy-constructs resource from the given \a other resource.
+*/
 ImageResource::ImageResource(const ImageResource &other)
 {
     construct(other);
 }
 
+/*!
+    Move-constructs resource from the given \a other resource.
+*/
 ImageResource::ImageResource(ImageResource &&other)
 {
     construct(std::move(other));
 }
 
+/*!
+    Destroys ImageResource object.
+*/
 ImageResource::~ImageResource()
 {
     destroy();
 }
 
+/*!
+    Assigns \a other ImageResource to this resource.
+*/
 ImageResource &ImageResource::operator=(const ImageResource &other)
 {
     if (this != &other) {
@@ -50,6 +93,9 @@ ImageResource &ImageResource::operator=(const ImageResource &other)
     return *this;
 }
 
+/*!
+    Move-assigns \a other ImageResource to this resource.
+*/
 ImageResource &ImageResource::operator=(ImageResource &&other)
 {
     if (this != &other) {
@@ -59,16 +105,26 @@ ImageResource &ImageResource::operator=(ImageResource &&other)
     return *this;
 }
 
+/*!
+    Returns true if resource doesn't contain any data and it's type is Type::Invalid.
+*/
 bool ImageResource::isNull() const Q_DECL_NOEXCEPT
 {
     return _type == Type::Invalid;
 }
 
+/*!
+    Returns resource type.
+*/
 ImageResource::Type ImageResource::type() const Q_DECL_NOEXCEPT
 {
     return _type;
 }
 
+/*!
+    Returns an image contained in the resource if it's type is Type::Image, otherwise null QImage is
+    returned.
+*/
 QImage ImageResource::image() const
 {
     if (_type != Type::Image)
@@ -76,6 +132,9 @@ QImage ImageResource::image() const
     return _image;
 }
 
+/*!
+    Sets data of this resource to the given \a image.
+*/
 void ImageResource::setImage(const QImage &image)
 {
     destroy();
@@ -83,6 +142,10 @@ void ImageResource::setImage(const QImage &image)
     _type = Type::Image;
 }
 
+/*!
+    Returns a CubeTexture contained in the resource if it's type is Type::CubeTexture, otherwise
+    null CubeTexture is returned.
+*/
 CubeTexture ImageResource::cubeTexture() const
 {
     if (_type != Type::CubeTexture)
@@ -90,6 +153,9 @@ CubeTexture ImageResource::cubeTexture() const
     return _cubeTexture;
 }
 
+/*!
+    Sets data of this resource to the given \a texture.
+*/
 void ImageResource::setCubeTexture(const CubeTexture &texture)
 {
     destroy();
@@ -97,6 +163,10 @@ void ImageResource::setCubeTexture(const CubeTexture &texture)
     _type = Type::CubeTexture;
 }
 
+/*!
+    Returns a VolumeTexture contained in the resource if it's type is Type::VolumeTexture, otherwise
+    null VolumeTexture is returned.
+*/
 VolumeTexture ImageResource::volumeTexture() const
 {
     if (_type != Type::VolumeTexture)
@@ -104,6 +174,9 @@ VolumeTexture ImageResource::volumeTexture() const
     return _volumeTexture;
 }
 
+/*!
+    Sets data of this resource to the given \a texture.
+*/
 void ImageResource::setVolumeTexture(const VolumeTexture &texture)
 {
     destroy();
@@ -111,6 +184,9 @@ void ImageResource::setVolumeTexture(const VolumeTexture &texture)
     _type = Type::CubeTexture;
 }
 
+/*!
+    \internal
+*/
 void ImageResource::construct(const ImageResource &other)
 {
     switch (other.type()) {
@@ -129,6 +205,9 @@ void ImageResource::construct(const ImageResource &other)
     _type = other._type;
 }
 
+/*!
+    \internal
+*/
 void ImageResource::construct(ImageResource &&other)
 {
     switch (other.type()) {
@@ -147,6 +226,9 @@ void ImageResource::construct(ImageResource &&other)
     _type = other._type;
 }
 
+/*!
+    \internal
+*/
 void ImageResource::destroy()
 {
     switch (_type) {
@@ -165,6 +247,10 @@ void ImageResource::destroy()
     _type = Type::Invalid;
 }
 
+/*!
+    Returns true if the \a lhs resource and the \a rhs resource have the same contents; otherwise
+    returns false.
+*/
 bool operator==(const ImageResource &lhs, const ImageResource &rhs)
 {
     if (lhs.type() != rhs.type())
@@ -182,6 +268,10 @@ bool operator==(const ImageResource &lhs, const ImageResource &rhs)
     return true;
 }
 
+/*!
+    Returns true if the \a lhs resource and the \a rhs resource have different contents; otherwise
+    returns false.
+*/
 bool operator!=(const ImageResource &lhs, const ImageResource &rhs)
 {
     return !(lhs == rhs);
