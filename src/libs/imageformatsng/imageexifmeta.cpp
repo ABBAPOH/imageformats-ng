ff4255ef099f4a3972b542cea93259b9aeb09e36
Ivan Komissarov <ABBAPOH@gmail.com>
2016-12-11 19:06:10 +0300

    Add doc for ImageExifMeta

--- src/libs/imageformatsng/imageexifmeta.cpp
+++ src/libs/imageformatsng/imageexifmeta.cpp
@@ -8,25 +8,95 @@ public:
     QHash<ImageExifMeta::Tag, QVariant> values;
 };
 
+/*!
+    \class ImageExifMeta
+    This class contains exif meta information of the image.
+*/
+
+/*!
+    \enum ImageExifMeta::Tag
+    This enum describes possible exif tags.
+
+    \var ImageExifMeta::TagImageWidth
+    Image width.
+
+    \var ImageExifMeta::TagImageHeight
+    Image heigth.
+
+    \var ImageExifMeta::TagDocumentName
+    Image name.
+
+    \var ImageExifMeta::TagImageDescription
+    Image description.
+
+    \var ImageExifMeta::TagOrientation
+    Image orientation. \sa ImageExifMeta::Orientation
+*/
+
+/*!
+    \enum ImageExifMeta::Orientation
+    This enum describes possible orientation of the image.
+
+    \var ImageExifMeta::OrientationHorizontal
+    Image is horizontal.
+
+    \var ImageExifMeta::OrientationHMirror
+    Image is horizontally mirrored.
+
+    \var ImageExifMeta::OrientationRotate180
+    Blah blah.
+
+    \var ImageExifMeta::OrientationVMirror180
+    Blah blah.
+
+    \var ImageExifMeta::OrientationHMirrorRotate270
+    Blah blah.
+
+    \var ImageExifMeta::OrientationRotate90
+    Blah blah.
+
+    \var ImageExifMeta::OrientationHMirrorRotate90
+    Blah blah.
+
+    \var ImageExifMeta::OrientationRotate270
+    Blah blah.
+*/
+
+/*!
+    Constructs an empty ImageExifMeta object.
+*/
 ImageExifMeta::ImageExifMeta() :
     d(new ImageExifMeta::Data)
 {
 }
 
+/*!
+    Copy-constructs an ImageExifMeta object from the given \a other object.
+*/
 ImageExifMeta::ImageExifMeta(const ImageExifMeta &other) :
     d(other.d)
 {
 }
 
+/*!
+    Move-constructs an ImageExifMeta object from the given \a other object.
+*/
 ImageExifMeta::ImageExifMeta(ImageExifMeta &&other) :
     d(qMove(other.d))
 {
 }
 
+/*!
+    Destroys the ImageExifMeta object.
+*/
 ImageExifMeta::~ImageExifMeta()
 {
 }
 
+/*!
+    Assigns a shallow copy of the \a other meta to this meta and returns a reference to this
+    meta.
+*/
 ImageExifMeta &ImageExifMeta::operator=(const ImageExifMeta &other)
 {
     if (this != &other)
@@ -34,6 +104,9 @@ ImageExifMeta &ImageExifMeta::operator=(const ImageExifMeta &other)
     return *this;
 }
 
+/*!
+    Move-assigns \a other to this ImageExifMeta instance.
+*/
 ImageExifMeta &ImageExifMeta::operator =(ImageExifMeta &&other)
 {
     if (this != &other)
@@ -41,11 +114,17 @@ ImageExifMeta &ImageExifMeta::operator =(ImageExifMeta &&other)
     return *this;
 }
 
+/*!
+    Converts this object to the QHash of containing QVariants.
+*/
 QHash<ImageExifMeta::Tag, QVariant> ImageExifMeta::toHash() const
 {
     return d->values;
 }
 
+/*!
+    Creates an ImageExifMeta object from the QHash containing QVariants.
+*/
 Optional<ImageExifMeta> ImageExifMeta::fromHash(const QHash<Tag, QVariant> &hash)
 {
     QScopedPointer<Data> dd(new Data);
@@ -55,6 +134,9 @@ Optional<ImageExifMeta> ImageExifMeta::fromHash(const QHash<Tag, QVariant> &hash
     return ImageExifMeta(dd.take());
 }
 
+/*!
+    Converts this object to the QMap of containing QVariants.
+*/
 QVariantMap ImageExifMeta::toVariantMap() const
 {
     QVariantMap result;
@@ -64,6 +146,9 @@ QVariantMap ImageExifMeta::toVariantMap() const
     return result;
 }
 
+/*!
+    Creates an ImageExifMeta object from the QMap containing QVariants.
+*/
 Optional<ImageExifMeta> ImageExifMeta::fromVariantMap(const QVariantMap &map)
 {
     QHash<Tag, QVariant> hash;
@@ -76,31 +161,49 @@ Optional<ImageExifMeta> ImageExifMeta::fromVariantMap(const QVariantMap &map)
     return fromHash(hash);
 }
 
+/*!
+    Returns true if this object contains no values.
+*/
 bool ImageExifMeta::isEmpty() const
 {
     return d->values.isEmpty();
 }
 
+/*!
+    Returns true if this object contains value for the given \a tag.
+*/
 bool ImageExifMeta::hasValue(ImageExifMeta::Tag tag) const
 {
     return d->values.contains(tag);
 }
 
-QVariant ImageExifMeta::value(Tag option) const
+/*!
+    Returns QVariant for the value contained in this object for the given \a tag.
+*/
+QVariant ImageExifMeta::value(Tag tag) const
 {
-    return d->values.value(option);
+    return d->values.value(tag);
 }
 
+/*!
+    Sets the value for the given \a tag to the given \a value.
+*/
 void ImageExifMeta::setValue(ImageExifMeta::Tag tag, const QVariant &value)
 {
     d->values.insert(tag, value);
 }
 
+/*!
+    Removes value for the given \a tag from this object.
+*/
 void ImageExifMeta::removeValue(ImageExifMeta::Tag tag)
 {
     d->values.remove(tag);
 }
 
+/*!
+    Cleares contents.
+*/
 void ImageExifMeta::clear()
 {
     d->values.clear();
